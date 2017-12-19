4e3c4641ddf4f2a7481d4d58975dec04addd1cdf
Mim van den Bos <mimnim1@gmail.com>
2017-12-19 22:18:41 +0100

    Fix mute position

--- src/renderers/mute-button.js
+++ src/renderers/mute-button.js
@@ -11,7 +11,7 @@ export class MuteButtonRenderer {
     ctx.ctx.font = '16px \'Segoe UI\', Tahoma, Geneva, Verdana, sans-serif';
     ctx.ctx.textAlign = 'right';
 
-    ctx.ctx.fillText('Mute', ctx.width - ctx.width * 0.05, ctx.height * 0.05);
+    ctx.ctx.fillText('Mute', ctx.width - ctx.scale * 0.05, ctx.scale * 0.05);
     if (Save.mute) {
       // TODO strikethrough
       ctx.ctx.fillRect();
