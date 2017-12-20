ad8875371289ca14c177aefad2bfcc1a09d2b03f
Mim van den Bos <mimnim1@gmail.com>
2017-12-20 18:20:07 +0100

    Add strikethrough on button when muted

--- src/renderers/mute-button.js
+++ src/renderers/mute-button.js
@@ -6,6 +6,24 @@ export class MuteButtonRenderer {
     this.text = 'Mute';
   }
 
+  getBoxPath(ctx, screenMargin, boxPadding, textWidth) {
+    const path = new Path2D();
+    const boxHeight = 16;
+
+    path.rect(ctx.width - screenMargin + boxPadding, screenMargin - boxPadding, -textWidth - boxPadding * 2, boxHeight + boxPadding * 2);
+
+    return path;
+  }
+
+  getStrikePath(ctx, screenMargin, textWidth) {
+    const path = new Path2D();
+
+    path.moveTo(ctx.width - screenMargin, screenMargin + 12);
+    path.lineTo(ctx.width - screenMargin - textWidth, screenMargin + 12);
+
+    return path;
+  }
+
   setTextStyle(ctx) {
     ctx.ctx.fillStyle = 'brown';
     ctx.ctx.font = '16px \'Segoe UI\', Tahoma, Geneva, Verdana, sans-serif';
@@ -21,23 +39,27 @@ export class MuteButtonRenderer {
 
   draw(ctx) {
     ctx.ctx.save();
-    this.setTextStyle(ctx);
-
-    const screenPadding = ctx.scale * 0.05;
+    
+    const screenMargin = ctx.scale * 0.05;
     const boxPadding = ctx.scale * 0.01;
-    const boxWidth = ctx.ctx.measureText(this.text).width;
-    const boxHeight = 16;
+
+    this.setTextStyle(ctx);
+    const textWidth = ctx.ctx.measureText(this.text).width;
+
+    const path = this.getBoxPath(ctx, screenMargin, boxPadding, textWidth);
 
     this.setBoxStyle(ctx);
-    ctx.ctx.fillRect(ctx.width - screenPadding + boxPadding, screenPadding - boxPadding, -boxWidth - boxPadding * 2, boxHeight + boxPadding * 2);
-    ctx.ctx.strokeRect(ctx.width - screenPadding + boxPadding, screenPadding - boxPadding, -boxWidth - boxPadding * 2, boxHeight + boxPadding * 2);
+    ctx.ctx.fill(path);
+    ctx.ctx.stroke(path);
 
     this.setTextStyle(ctx);
-    ctx.ctx.fillText(this.text, ctx.width - screenPadding, screenPadding);
+    ctx.ctx.fillText(this.text, ctx.width - screenMargin, screenMargin);
 
     // Strikethrough text if muted
     if (Save.mute) {
-      ctx.ctx.fillRect();
+      this.setBoxStyle(ctx);
+      const strokePath = this.getStrikePath(ctx, screenMargin, textWidth);
+      ctx.ctx.stroke(strokePath);
     }
 
     ctx.ctx.restore();
